3cfa2c2ee63a967fb3d98d78ae5444956f5939d1
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-11-30 16:06:16 -0300

    remove comments

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (!newTaskTitle) return
     
     const newTask = {
@@ -29,7 +28,6 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     const myTasks = tasks.map(task => task.id === id ? {
       ...task,
       isComplete: !task.isComplete
@@ -38,7 +36,6 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
     setTasks(tasks.filter(task => task.id !== id))
   }
 
