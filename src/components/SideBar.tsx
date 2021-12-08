2a129139c032053b20b0eae1e95ace6891e87345
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2021-12-07 21:34:20 -0300

    separating components to make the code more organized

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+import { api } from "../services/api";
+import { Button } from "./Button"
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SiderBarProps{
+  selectedGenreId: number
+  setSelectedGenreId: (id: number) => void
+}
+
+export function SideBar({ selectedGenreId, setSelectedGenreId }: SiderBarProps) {
+  
+  const [genres, setGenres] = useState<GenreResponseProps[]>([])
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id)
+  }
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
+}
